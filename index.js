f0909bd7890bda600f7961d9127b079b7de0064b
Naruepanart Siangsanan <naruepanart1201@gmail.com>
2022-10-15 16:05:16 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const port = 3001;
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+  socket.on("disconnect", () => {
+    console.log("disconnect");
+  });
+
+  socket.on("join-room", (data) => {
+    console.log("join-room")
+    socket.join(data.id);
+  });
+  
+  socket.on("send-message", (data) => {
+    console.log("send-message")
+    socket.to(data.room).emit("rec", data);
+  });
+});
+
+server.listen(port, () => console.log(`http://localhost:${port}`));
